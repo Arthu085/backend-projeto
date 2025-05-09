281e1fd2869b4a3bb4d2a6b97cd63ec1257099ef
Arthu085 <arthurghizi22@gmail.com>
2025-05-08 21:44:01 -0300

    ajustado readme e alguns erros

--- backend/src/main/java/com/example/backend_projeto/models/User.java
+++ backend/src/main/java/com/example/backend_projeto/models/User.java
@@ -44,6 +44,12 @@ public class User implements UserDetails {
     }
 
     @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+
+    @Override
     public String getUsername() {
         return this.email; // ou qualquer outro identificador único
     }
